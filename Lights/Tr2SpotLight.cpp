333b7ffda766be2e937a6c9d3bd222319b76d9ad
ccpnobody <55136070+ccpnobody@users.noreply.github.com>
2020-02-06 10:53:57 +0000

    [eve MAINLINE] fixing the debug representation of spotlight (to take scale into account)
    https://jira.ccpgames.com/browse/EVEDEV-9372
    
    [git-p4: depot-paths = "//eve/branches/development/MAINLINE/carbon/src/Trinity/": change = 1661258]

--- Lights/Tr2SpotLight.cpp
+++ Lights/Tr2SpotLight.cpp
@@ -22,12 +22,21 @@ void Tr2SpotLight::RenderDebugInfo( ITr2DebugRenderer2& renderer, const Matrix& 
 	auto baseColor = m_lightData.color * m_lightData.brightness;
 	baseColor.a = 0.1;
 	auto selectedColor = baseColor + Color( 0.0, 0.0, 0.0, 0.1 );
+	
+	auto lightRotation = RotationMatrix( Normalize( m_lightData.rotation * RotationQuaternion( worldMatrix ) ) );
+	auto translationMatrix = TranslationMatrix( worldMatrix.GetTranslation() );
+	float scaling = XMVectorGetX( XMVectorAdd( XMVector3LengthEst( worldMatrix.GetX() ),
+		XMVectorAdd( XMVector3LengthEst( worldMatrix.GetY() ), XMVector3LengthEst( worldMatrix.GetZ() ) ) ) ) / 3.f;
 
-	Vector3 end = Transform( Vector3( 0.0, 0.0, m_lightData.radius ), RotationMatrix( m_lightData.rotation ) ).GetXYZ();
-	end = TransformCoord( end + m_lightData.position, worldMatrix );
-	Vector3 start = TransformCoord( m_lightData.position, worldMatrix );
-	float outerConeRadius = tan( TRI_2PI * m_lightData.outerAngle / 360.f ) * m_lightData.radius;
-	float innerConeRadius = tan( TRI_2PI * m_lightData.innerAngle / 360.f ) * m_lightData.radius;
-	renderer.DrawCone( this, end, start, outerConeRadius, 10, Tr2DebugRenderer::Solid, Tr2DebugColor( selectedColor, baseColor ) );
-	renderer.DrawCone( this, end, start, innerConeRadius, 10, Tr2DebugRenderer::Solid, Tr2DebugColor( selectedColor, baseColor ) );
+	Vector3 outerEnd = Transform( Vector3( 0.0, 0.0, m_lightData.radius * scaling ), lightRotation ).GetXYZ();
+	outerEnd = TransformCoord( outerEnd + m_lightData.position, translationMatrix );
+
+	Vector3 innerEnd = Transform( Vector3( 0.0, 0.0, m_lightData.innerRadius * scaling ), lightRotation ).GetXYZ();
+	innerEnd = TransformCoord( innerEnd + m_lightData.position, translationMatrix );
+
+	Vector3 start = TransformCoord( m_lightData.position, translationMatrix );
+	float outerConeRadius = tan( TRI_2PI * m_lightData.outerAngle / 360.f ) * m_lightData.radius * scaling;
+	float innerConeRadius = tan( TRI_2PI * m_lightData.innerAngle / 360.f ) * m_lightData.innerRadius * scaling;
+	renderer.DrawCone( this, outerEnd, start, outerConeRadius, 10, Tr2DebugRenderer::Solid, Tr2DebugColor( selectedColor, baseColor ) );
+	renderer.DrawCone( this, innerEnd, start, innerConeRadius, 10, Tr2DebugRenderer::Solid, Tr2DebugColor( selectedColor, baseColor ) );
 }
