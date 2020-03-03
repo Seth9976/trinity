3f6422965a61ae28dc19ea248dc04a425fc4aa47
Henrý Þór Jónsson <henry@ccpgames.com>
2020-03-03 11:13:08 +0000

    Undo the undo since TeamCity was apperently breaking for some other reason //eve/branches/development/MAINLINE/carbon/src/trinity/Eve/... changelist 1677851
    [git-p4: depot-paths = "//eve/branches/development/MAINLINE/carbon/src/Trinity/": change = 1678715]

--- Eve/EveEffectRoot2.cpp
+++ Eve/EveEffectRoot2.cpp
@@ -88,6 +88,7 @@ void EveEffectRoot2::OnListModified( long event, ssize_t key, ssize_t key2, IRoo
 				{
 					child->SetControllerVariable( it->first.c_str(), it->second );
 				}
+				child->StartControllers();
 			}
 			break;
 		default:
